ed404040e8da0e76e9e6a317e8693d8d76be8bbb
PaulAshraf <paulashraf14@gmail.com>
2020-11-07 07:31:50 +0200

    fix warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import TaskList from './components/TaskList'
 import Header from './components/Header'
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
 
 function App() {
 	return (
